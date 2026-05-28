ca52bcd3f922c551e531fec3e46eae0f3c76ca30
Bernardo <bernardobarbosalima@gmail.com>
2026-05-28 15:23:00 +0100

    Bernardo gado

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-def ninja():
-    print("hello Ninja")
-    
-ninja()    
+# This function calculates the area of a triangle.
+def triangle_area(base, height):
+    total_area = (base * height)/2
+    return total_area
+
+
+base = int(input("Enter the base of the triangle: "))
+height = int(input("Enter the height of the triangle: "))
+area = triangle_area(base, height)
+print(f"The area of the triangle is: {area}")
